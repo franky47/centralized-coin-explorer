0cd90be0a5d4d89a7e0dbee1197a46c4982b846b
mustardpower <paul_gothard93@hotmail.co.uk>
2021-04-27 13:18:53 +0100

    Add first and last buttons to transaction page
    Can now navigate to first and last page via two additional buttons. Might need some limitation to prevent navigation past the last page?

--- src/client/components/PageNavigation.tsx
+++ src/client/components/PageNavigation.tsx
@@ -2,7 +2,9 @@ import { Button, Flex, FlexProps, Text } from '@chakra-ui/react'
 import { queryTypes, useQueryState } from 'next-usequerystate'
 import React from 'react'
 import { FiArrowLeft, FiArrowRight } from 'react-icons/fi'
+import { MdFirstPage, MdLastPage} from 'react-icons/md'
 import { useTransactionPage } from 'src/services/centralized-coin'
+import { getNumberOfPages } from 'src/services/centralized-coin'
 
 // --
 
@@ -11,17 +13,26 @@ function useNavigation() {
     defaultValue: 0,
     ...queryTypes.integer
   })
+  
+  const last = React.useCallback(() => getNumberOfPages().then(
+    (value: number) => { setPage(page => value)
+  }), [])
 
   const next = React.useCallback(() => setPage(page => page + 1), [])
   const prev = React.useCallback(
     () => setPage(page => Math.max(0, page - 1)),
     []
   )
+
+  const first = React.useCallback(() => setPage(page => 0), [])
+
   const { isLoading, isFetching } = useTransactionPage(page)
   return {
     page,
+    last,
     next,
     prev,
+    first,
     setPage,
     isLoading: isLoading || isFetching
   }
@@ -32,9 +43,20 @@ function useNavigation() {
 export interface PageNavigationProps extends FlexProps {}
 
 export const PageNavigation: React.FC<PageNavigationProps> = ({ ...props }) => {
-  const { page, next, prev, isLoading } = useNavigation()
+
+  const { page, last, next, prev, first, isLoading } = useNavigation()
   return (
     <Flex alignItems="center" {...props}>
+      <Button
+        onClick={first}
+        isDisabled={page === 0}
+        leftIcon={<MdFirstPage />}
+        isLoading={isLoading}
+        w="7.5rem"
+      >
+        {' '}
+        First
+      </Button>
       <Button
         onClick={prev}
         isDisabled={page === 0}
@@ -55,6 +77,14 @@ export const PageNavigation: React.FC<PageNavigationProps> = ({ ...props }) => {
         isLoading={isLoading}
       >
         Next
+      </Button>
+      <Button
+        onClick={last}
+        rightIcon={<MdLastPage />}
+        w="7.5rem"
+        isLoading={isLoading}
+      >
+        Last
       </Button>
     </Flex>
   )
